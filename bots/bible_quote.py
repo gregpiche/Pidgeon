3300d7f786d3ba4df9edb281aab7ab29dbea0389
Gregory Piche <gregorypiche@Gregorys-MacBook-Pro.local>
2022-01-26 16:10:35 -0500

    Bible quote bot V1

---
+++ bots/bible_quote.py
@@ -0,0 +1,29 @@
+from email.quoprimime import quote
+import requests
+import json
+import os
+from utils.sms import send_sms 
+from utils.database import get
+
+# Build request
+url = "https://uncovered-treasure-v1.p.rapidapi.com/today"
+
+headers = {
+    'x-rapidapi-host': "uncovered-treasure-v1.p.rapidapi.com",
+    'x-rapidapi-key': os.getenv('UNCOVERED_TREASURE_KEY')
+    }
+
+# Make request for bible quote
+response = requests.request("GET", url, headers=headers)
+response = json.loads(response.text)['results'][0]
+
+# Parse response
+context = response['context']
+quote = response['text']
+body = context + '\n' + quote
+print(body)
+
+users = get('bible_quote')
+
+# Send SMS
+send_sms(body, users)
